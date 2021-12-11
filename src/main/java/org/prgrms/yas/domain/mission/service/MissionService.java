1054e6c434ccf59bd6b1b8a5001fa63cce2c7a4b
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2021-12-12 00:56:01 +0900

    Refactor : 루틴에 대한 예외처리 미션 예외로 되어있던거 수정

--- src/main/java/org/prgrms/yas/domain/mission/service/MissionService.java
+++ src/main/java/org/prgrms/yas/domain/mission/service/MissionService.java
@@ -6,6 +6,7 @@ import org.prgrms.yas.domain.mission.dto.MissionCreateRequest;
 import org.prgrms.yas.domain.mission.exception.NotFoundMissionException;
 import org.prgrms.yas.domain.mission.repository.MissionRepository;
 import org.prgrms.yas.domain.routine.domain.Routine;
+import org.prgrms.yas.domain.routine.exception.NotFoundRoutineException;
 import org.prgrms.yas.domain.routine.repository.RoutineRepository;
 import org.prgrms.yas.global.error.ErrorCode;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class MissionService {
 			Long routineId, MissionCreateRequest missionCreateRequest
 	) {
 		Routine routine = routineRepository.findById(routineId)
-		                                   .orElseThrow(() -> new NotFoundMissionException(ErrorCode.NOT_FOUND_RESOURCE_ERROR));
+		                                   .orElseThrow(() -> new NotFoundRoutineException(ErrorCode.NOT_FOUND_RESOURCE_ERROR));
 		Mission mission = Mission.builder()
 		                         .name(missionCreateRequest.getName())
 		                         .durationGoalTime(missionCreateRequest.getDurationGoalTime())
