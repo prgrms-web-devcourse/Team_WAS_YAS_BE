e23c46686cbde2a40d3eaa1a6931a30747f69791
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2021-12-12 02:58:11 +0900

    Feat : 미션 삭제 기능 추가

--- src/main/java/org/prgrms/yas/domain/mission/controller/MissionController.java
+++ src/main/java/org/prgrms/yas/domain/mission/controller/MissionController.java
@@ -4,9 +4,11 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.prgrms.yas.domain.mission.dto.MissionCreateRequest;
 import org.prgrms.yas.domain.mission.service.MissionService;
+import org.prgrms.yas.domain.routine.dto.RoutineDeleteResponse;
 import org.prgrms.yas.global.response.ApiResponse;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,13 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/routines/{id}/missions")
+@RequestMapping(value = "/routines")
 @RequiredArgsConstructor
 public class MissionController {
 	
 	private final MissionService missionService;
 	
-	@PostMapping
+	@PostMapping("/{id}/missions")
 	public ResponseEntity<ApiResponse<Long>> create(
 			@Valid @RequestBody MissionCreateRequest missionCreateRequest,
 			@PathVariable("id") Long routineId
@@ -30,5 +32,11 @@ public class MissionController {
 				missionCreateRequest
 		);
 		return ResponseEntity.ok(ApiResponse.of(missionId));
+	}
+	
+	@DeleteMapping("missions/{id}")
+	public ResponseEntity<ApiResponse<Long>> delete(@PathVariable("id") Long missionId) {
+		Long deleteMissionId = missionService.deleteMission(missionId);
+		return ResponseEntity.ok(ApiResponse.of(deleteMissionId));
 	}
 }
