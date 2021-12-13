11dd79a61c4b9b33fbc88a5a857b7a188efe338b
bnminji <bnminji@gmail.com>
2021-12-14 02:54:44 +0900

    Refactor : 댓글 PR리뷰 반영, BaseEntity Auditing 추가

--- src/main/java/org/prgrms/yas/domain/comment/controller/CommentController.java
+++ src/main/java/org/prgrms/yas/domain/comment/controller/CommentController.java
@@ -12,6 +12,7 @@ import org.prgrms.yas.global.response.ApiResponse;
 import org.prgrms.yas.jwt.JwtAuthentication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,43 +24,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
-
-  private final CommentService commentService;
-
-  //댓글 등록
-  @Operation(summary = "댓글 등록 컨트롤러")
-  @PostMapping("/post/{id}/comment")
-  public ResponseEntity<ApiResponse<Long>> create(
-      final @AuthenticationPrincipal JwtAuthentication token, final @PathVariable Long postId,
-      final @RequestBody CommentCreateRequest commentCreateRequest
-  ) throws NotFoundUserException, NotFoundRoutineException {
-    return ResponseEntity.ok(ApiResponse.of(commentService.saveComment(
-        token.getId(),
-        postId,
-        commentCreateRequest
-    )));
-  }
-
-  //댓글 수정
-  @Operation(summary = "댓글 수정 컨트롤러")
-  @PatchMapping("/comment/{commentId}/update")
-  public ResponseEntity<ApiResponse<Long>> update(
-      final @PathVariable Long commentId,
-      final @RequestBody CommentUpdateRequest commentUpdateRequest
-  ) throws NotFoundCommentException {
-    return ResponseEntity.ok(ApiResponse.of(commentService.updateComment(
-        commentId,
-        commentUpdateRequest
-    )));
-  }
-
-  //댓글 삭제
-  @Operation(summary = "댓글 삭제 컨트롤러")
-  @PatchMapping("/comment/{commentId}/delete")
-  public ResponseEntity<ApiResponse<Long>> delete(
-      final @PathVariable Long commentId
-  ) throws NotFoundCommentException {
-    return ResponseEntity.ok(ApiResponse.of(commentService.deleteComment(commentId)));
-  }
-
+	
+	private final CommentService commentService;
+	
+	@Operation(summary = "댓글 등록 컨트롤러")
+	@PostMapping("/post/{postId}/comment")
+	public ResponseEntity<ApiResponse<Long>> create(
+			final @AuthenticationPrincipal JwtAuthentication token, final @PathVariable Long postId,
+			final @RequestBody CommentCreateRequest commentCreateRequest
+	) {
+		return ResponseEntity.ok(ApiResponse.of(commentService.saveComment(
+				token.getId(),
+				postId,
+				commentCreateRequest
+		)));
+	}
+	
+	@Operation(summary = "댓글 수정 컨트롤러")
+	@PatchMapping("/comment/{commentId}")
+	public ResponseEntity<ApiResponse<Long>> update(
+			final @PathVariable Long commentId,
+			final @RequestBody CommentUpdateRequest commentUpdateRequest
+	) {
+		return ResponseEntity.ok(ApiResponse.of(commentService.updateComment(
+				commentId,
+				commentUpdateRequest
+		)));
+	}
+	
+	@Operation(summary = "댓글 삭제 컨트롤러")
+	@DeleteMapping("/comment/{commentId}")
+	public ResponseEntity<ApiResponse<Long>> delete(
+			final @PathVariable Long commentId
+	) {
+		return ResponseEntity.ok(ApiResponse.of(commentService.deleteComment(commentId)));
+	}
+	
 }
