1455ddb0d3bfb6c7bfb790bb1062386f00c52408
Suhyeong Lee <skyey9489@gmail.com>
2021-12-15 04:34:03 +0900

    Chore : WebSecurityConfig import 수정

--- src/main/java/org/prgrms/yas/configures/WebSecurityConfig.java
+++ src/main/java/org/prgrms/yas/configures/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.prgrms.yas.oauth2.OAuth2AuthenticationSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
