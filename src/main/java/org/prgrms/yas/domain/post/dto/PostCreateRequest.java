1b4f527e416ec61c41194bda4ead63530f5a180c
LSH0809 <skyey94@naver.com>
2021-12-19 23:45:33 +0900

    Fix : 게시물 생성 DTO 수정
    - 기본 생성자 추가

--- src/main/java/org/prgrms/yas/domain/post/dto/PostCreateRequest.java
+++ src/main/java/org/prgrms/yas/domain/post/dto/PostCreateRequest.java
@@ -1,10 +1,13 @@
 package org.prgrms.yas.domain.post.dto;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostCreateRequest {
-	private final String content;
+	private String content;
 	
 	public PostCreateRequest(String content) {
 		this.content = content;
