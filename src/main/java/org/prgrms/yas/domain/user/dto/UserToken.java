09beaa1ce96418303f6d2c8d05e79fde3bb0c606
LSH0809 <skyey94@naver.com>
2022-01-18 17:25:06 +0900

    Refactor : JWT 토큰 구조 변경 - 로그인 이슈 해결 - JWT 토큰 이메일 필드 삭제

--- src/main/java/org/prgrms/yas/domain/user/dto/UserToken.java
+++ src/main/java/org/prgrms/yas/domain/user/dto/UserToken.java
@@ -7,13 +7,11 @@ public class UserToken {
 	
 	private final Long id;
 	private final String token;
-	private final String username;
 	private final String roles;
 	
-	public UserToken(Long id, String token, String username, String roles) {
+	public UserToken(Long id, String token, String roles) {
 		this.id = id;
 		this.token = token;
-		this.username = username;
 		this.roles = roles;
 	}
 }
